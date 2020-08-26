9a8dfe56009cb0c7b561e2746eaff6abb12b6c86
Charles Kallick <67604746+charleskallick@users.noreply.github.com>
2020-08-26 15:15:54 -0700

    track file update and first instance

---
+++ src/Components/Tracks/Track.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Track.css';
+
+class Tracks extends React.Component() {
+  
+  renderAction() {
+    if (isRemoval !== true) {
+      return (
+        <div>
+          <button>+</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button>-</button>
+        </div>
+      );
+    }
+  };
+  
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>/*track name will go here*/</h3>
+          <p>/*track artist will go here*/ | /*track album will go here*/</p>
+        </div>
+        <button className="Track-action">{this.renderAction}</button>
+      </div>
+    );
+  }
+};
+
+module.exports = Tracks;
