4d1d14585fb6ed119dde483af355107ccf4cc3c5
Charles Kallick <67604746+charleskallick@users.noreply.github.com>
2020-08-26 16:25:07 -0700

    minor fixes

--- src/Components/app.js
+++ src/Components/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 
 import SearchBar from './SearchBar/SearchBar';
-import SearchResults from './SearchResults/SearchResults';
+import SearchResults from 'SearchResults/SearchResults';
 import Playlist from './Playlist/Playlist';
 
 class App extends React.Component() {
   constructor(props) {
     super(props);
     this.state = { 
-      searchResults: [{name1: 'name1', artist1: 'artist1', album1: 'album1', id=1}, {name2: 'name2', artist2: 'artist2', album2: 'album2', id=2}, {name3: 'name3', artist3: 'artist3', album3: 'album3', id=3}]
+      searchResults: [{name1: 'name1', artist1: 'artist1', album1: 'album1', id: 1}, {name2: 'name2', artist2: 'artist2', album2: 'album2', id: 2}, {name3: 'name3', artist3: 'artist3', album3: 'album3', id: 3}]
     }
   };
 
