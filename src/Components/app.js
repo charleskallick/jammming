a538bc9d566bb9c89faf4504481a44b989e38e27
Charles Kallick <67604746+charleskallick@users.noreply.github.com>
2020-08-27 10:30:09 -0700

    spotify search method and start to playlist method

--- src/Components/app.js
+++ src/Components/app.js
@@ -51,7 +51,9 @@ class App extends React.Component {
   };
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
   };
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -24,8 +24,46 @@ const Spotify = {
       const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
       window.location.accessURL;
     }
+  },
 
-  }
+  search(term) {
+    const accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+        }
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map(track => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri
+        }));     
+      });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = {Authorization: `Bearer ${accessToken}`};
+    let userID;
+
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userID = jsonResponse.id;
+      return fetch(``)
+    });
+  },
 
 };
 
